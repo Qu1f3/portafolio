0d45fa99ece02f92620a854e37400ec8469d8bdd
Jose Roberto Quiroz <154577631+Qu1f3@users.noreply.github.com>
2025-06-21 20:05:11 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -28,6 +28,7 @@ const getInput = () => {
 export default defineConfig(
     {
         appType: 'mpa',
+        base: process.env.DEPLOY_BASE_URL,
         build: {
             rollupOptions: {
                 input: getInput()
