bbbdf9dbc6eeeac3ba28603cdd3f89a6ddf93476
Roberto Quiroz <quirozroberto293@gmail.com>
2025-06-21 20:29:23 -0600

    Subir Portafolio

--- vite.config.js
+++ vite.config.js
@@ -36,7 +36,7 @@ export default defineConfig(
             minify: true
         },
         plugins: [
-           // htmlPurge({}),
+           /// htmlPurge({}),
             ViteMinifyPlugin(),
         ]
     }
